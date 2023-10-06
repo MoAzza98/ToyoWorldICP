60592f0098dc547fd624175a6bea3d576b0e8960
Mo Azza <ssbsdmg@gmail.com>
2023-10-06 22:16:07 +0100

    Island backup

--- ToyoWorld/Assets/Scripts/PlayerInteract.cs
+++ ToyoWorld/Assets/Scripts/PlayerInteract.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerInteract : MonoBehaviour
 {
     public float interactRange = 2f;
+    //public ToyoParty party { get; set; }
 
     // Start is called before the first frame update
     void Start()
